a0a68afde1b6782f00c3d31f0378c851d8a71de8
SuperSerg58 <demiurg58@gmail.com>
2020-09-15 12:09:11 +0300

    'add_utils_and_handlers'

--- bot.py
+++ bot.py
@@ -1,77 +1,14 @@
-from glob import glob
-from random import choice
 import logging
-
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, RegexHandler
-from telegram import ReplyKeyboardMarkup, KeyboardButton
-from emoji import emojize
 import settings
+
+from handlers import greeting_user, send_cat_picture, change_avatar, get_contact, get_location, talk_to_me
+
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log'
                     )
-
-
-def greeting_user(bot, update, user_data):
-    emo = get_user_emo(user_data)
-    user_data['emo'] = emo
-    text = f'Hello {emo}'
-
-    update.message.reply_text(text, reply_markup=get_keyboard())
-
-
-def talk_to_me(bot, update, user_data):
-    emo = get_user_emo(user_data)
-    user_text = f"Привет {update.message.chat.first_name}{emo}! Ты написал: {update.message.text}"
-    logging.info("User: %s, Chat id: %s, Message: %s", update.message.chat.username,
-                 update.message.chat.id,
-                 update.message.text)
-    update.message.reply_text(user_text, reply_markup=get_keyboard())
-
-
-def send_cat_picture(bot, update, user_data):
-    cat_list = glob('images\*cat*.jp*g')
-    cat_pic = choice(cat_list)
-    bot.send_photo(chat_id=update.message.chat.id, photo=open(cat_pic, 'rb'), reply_markup=get_keyboard())
-
-
-def change_avatar(bot, update, user_data):
-    if 'emo' in user_data:
-        del user_data['emo']
-    emo = get_user_emo(user_data)
-    update.message.reply_text(f'Done{emo}', reply_markup=get_keyboard())
-
-
-def get_user_emo(user_data):
-    if 'emo' in user_data:
-        return user_data['emo']
-    else:
-        user_data['emo'] = emojize(choice(settings.USER_EMOJI), use_aliases=True)
-        return user_data['emo']
-
-
-def get_contact(bot, update, user_data):
-    print(update.message.contact)
-    update.message.reply_text(f'Done{get_user_emo(user_data)}', reply_markup=get_keyboard())
-
-
-def get_location(bot, update, user_data):
-    print(update.message.location)
-    update.message.reply_text(f'Done{get_user_emo(user_data)}', reply_markup=get_keyboard())
-
-
-def get_keyboard():
-    contact_button = KeyboardButton('Прислать контакты', request_contact=True)
-    location_button = KeyboardButton('Прислать координаты', request_location=True)
-    my_keyboard = ReplyKeyboardMarkup(
-        [
-            ['Прислать котика', 'Сменить аватарку'],
-            [contact_button, location_button]
-        ],
-        resize_keyboard=True
-    )
-    return my_keyboard
 
 
 def main():
@@ -83,8 +20,8 @@ def main():
     # комманды которые вводит юзер в боте
     dp.add_handler(CommandHandler("start", greeting_user, pass_user_data=True))
     dp.add_handler(CommandHandler("cat", send_cat_picture, pass_user_data=True, ))
-    dp.add_handler(RegexHandler('^(Прислать котика)$', send_cat_picture, pass_user_data=True))
-    dp.add_handler(RegexHandler('^(Сменить аватарку)$', change_avatar, pass_user_data=True))
+    dp.add_handler(MessageHandler(Filters.regex('Прислать котика'), send_cat_picture, pass_user_data=True))
+    dp.add_handler(MessageHandler(Filters.regex('Сменить аватарку'), change_avatar, pass_user_data=True))
     dp.add_handler(MessageHandler(Filters.contact, get_contact, pass_user_data=True))
     dp.add_handler(MessageHandler(Filters.location, get_location, pass_user_data=True))
 
